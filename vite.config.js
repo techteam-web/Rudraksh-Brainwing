ddb5433d874a97da2542fb0ba1768751a9b2558b
Crimson2424 <priyankmiles@gmail.com>
2026-03-12 11:20:13 +0530

    Fix for vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/SalesTool/Rudraksh/' : '/',
+  // base: command === 'build' ? '/SalesTool/Rudraksh/' : '/',
   plugins: [tailwindcss(), react()],
 }))
